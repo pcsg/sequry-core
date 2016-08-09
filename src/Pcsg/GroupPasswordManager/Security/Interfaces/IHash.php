656cd92b3cea852c56fd3228d48dfbc2338239e6
Patrick Müller <p.mueller@pcsg.de>
2016-08-09 14:15:48 +0200

    refactor: psr fix

---
+++ src/Pcsg/GroupPasswordManager/Security/Interfaces/IHash.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pcsg\GroupPasswordManager\Security\Interfaces;
+
+/**
+ * This class provides a Hash API for the pcsg/grouppasswordmanager module
+ */
+interface IHash
+{
+    /**
+     * Creates a hash
+     *
+     * @param string $str - A String
+     * @param string $salt (optional) - if omitted genereate random hash
+     * @return string - The hash
+     */
+    public static function create($str, $salt = null);
+}
