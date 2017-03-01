1104c9f35f820b096ee24ae4429097b8b0ce058e
Patrick Müller <p.mueller@pcsg.de>
2017-03-01 18:25:27 +0100

    style: flex table anpassungen

--- bin/controls/passwordtypes/Edit.js
+++ bin/controls/passwordtypes/Edit.js
@@ -134,6 +134,31 @@ define('package/pcsg/grouppasswordmanager/bin/controls/passwordtypes/Edit', [
             var Elm;
             var showHideElms = this.$Elm.getElements('.pwm-passwordtypes-show');
 
+            var FuncOnShowBtnClick = function (Btn) {
+                var Elm = Btn.getAttribute('Elm');
+
+                if (Btn.getAttribute('action') === 'show') {
+                    Btn.setAttributes({
+                        icon  : 'fa fa-eye-slash',
+                        action: 'hide'
+                    });
+
+                    Elm.setProperty('type', 'text');
+                    Elm.focus();
+                    Elm.select();
+
+                    return;
+                }
+
+                Btn.setAttributes({
+                    icon  : 'fa fa-eye',
+                    action: 'show'
+                });
+
+                Elm.setProperty('type', 'password');
+                Elm.blur();
+            };
+
             for (var i = 0, len = showHideElms.length; i < len; i++) {
                 Elm = showHideElms[i];
 
@@ -144,35 +169,17 @@ define('package/pcsg/grouppasswordmanager/bin/controls/passwordtypes/Edit', [
                     title : QUILocale.get(lg, 'controls.passwordtypes.edit.show.btn'),
                     action: 'show',
                     events: {
-                        onClick: function (Btn) {
-                            var Elm = Btn.getAttribute('Elm');
-
-                            if (Btn.getAttribute('action') === 'show') {
-                                Btn.setAttributes({
-                                    icon  : 'fa fa-eye-slash',
-                                    action: 'hide'
-                                });
-
-                                Elm.setProperty('type', 'text');
-                                Elm.focus();
-                                Elm.select();
-
-                                return;
-                            }
-
-                            Btn.setAttributes({
-                                icon  : 'fa fa-eye',
-                                action: 'show'
-                            });
-
-                            Elm.setProperty('type', 'password');
-                            Elm.blur();
-                        }
+                        onClick: FuncOnShowBtnClick
                     }
-                }).inject(Elm.getParent(), 'after');
+                }).inject(Elm.getParent());
             }
 
             var rndPassElms = this.$Elm.getElements('.pwm-passwordtypes-randompassword');
+
+            var FuncOnRndBtnClick = function (Btn) {
+                var Elm   = Btn.getAttribute('Elm');
+                Elm.value = Math.random().toString(36).slice(-16);
+            };
 
             for (i = 0, len = rndPassElms.length; i < len; i++) {
                 Elm = rndPassElms[i];
@@ -183,12 +190,9 @@ define('package/pcsg/grouppasswordmanager/bin/controls/passwordtypes/Edit', [
                     title : QUILocale.get(lg, 'controls.passwordtypes.edit.rnd.btn'),
                     icon  : 'fa fa-random',
                     events: {
-                        onClick: function (Btn) {
-                            var Elm   = Btn.getAttribute('Elm');
-                            Elm.value = Math.random().toString(36).slice(-16);
-                        }
+                        onClick: FuncOnRndBtnClick
                     }
-                }).inject(Elm.getParent(), 'after');
+                }).inject(Elm.getParent());
             }
 
             this.fireEvent('loaded', [this]);
