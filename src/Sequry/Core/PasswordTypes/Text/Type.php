5b8e755dbe3a5765c4c96cd6070b09a32d63fd25
Patrick Müller <p.mueller@pcsg.de>
2018-05-28 12:34:52 +0200

    feat: Text password type

---
+++ src/Sequry/Core/PasswordTypes/Text/Type.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Sequry\Core\PasswordTypes\Text;
+
+use Sequry\Core\PasswordTypes\TemplateUtils;
+use QUI;
+use Sequry\Core\PasswordTypes\IPasswordType;
+
+/**
+ * Type class for Text password input type
+ */
+class Type implements IPasswordType
+{
+    /**
+     * Get view template
+     *
+     * @param array $content (optional) - the content that is parsed into the template
+     * @return string - HTML template
+     */
+    public static function getViewHtml($content = array())
+    {
+        $content = array_merge($content, self::getTemplateTranslations());
+        return TemplateUtils::parseTemplate(dirname(__FILE__) . '/View.html', $content, true);
+    }
+
+    /**
+     * Get edit template (just HTML)
+     *
+     * @return string - HTML template
+     */
+    public static function getEditHtml()
+    {
+        $content = self::getTemplateTranslations();
+        return TemplateUtils::parseTemplate(dirname(__FILE__) . '/Edit.html', $content);
+    }
+
+    /**
+     * Return template translations
+     *
+     * @return array
+     */
+    protected static function getTemplateTranslations()
+    {
+        $L        = QUI::getLocale();
+        $lg       = 'sequry/core';
+        $lgPrefix = 'passwordtypes.text.label.';
+
+        return array(
+            'labelText'  => $L->get($lg, $lgPrefix . 'text'),
+            'labelTitle' => $L->get($lg, $lgPrefix . 'title')
+        );
+    }
+
+    /**
+     * Get content that is copied by a copy action
+     *
+     * @param array $payload - password payload
+     * @return string - copy content
+     */
+    public static function getCopyContent($payload)
+    {
+        if (isset($payload['text'])) {
+            return $payload['text'];
+        }
+
+        return '';
+    }
+}
